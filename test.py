4a5a4666c71310b7ec2eae1deb611ec89a58642f
ash-og <aisling.young4@gmail.com>
2024-03-29 17:27:04 +0000

    test fully working

--- test.py
+++ test.py
@@ -12,8 +12,6 @@ class TestAes(unittest.TestCase):
         # Prepare the input buffer
         buffer = b'\x00\x01\x02\x03\x04\x05\x06\x07'
         buffer += b'\x08\x09\x0A\x0B\x0C\x0D\x0E\x0F'
-        buffer2 = b'\x00\x01\x01\x03\x04\x05\x06\x07'
-        buffer2 += b'\x08\x09\x0A\x0B\x0C\x0D\x0E\x0F'
         block = ctypes.create_string_buffer(buffer)
 
         # Call the C sub_bytes function
@@ -21,7 +19,7 @@ class TestAes(unittest.TestCase):
 
 
         # Prepare the same input for the Python implementation
-        p_block = bytes2matrix(buffer2)
+        p_block = bytes2matrix(buffer)
         # result_p = python_sub_bytes(bytes(p_block))
         python_sub_bytes(p_block)
         # Prepare the output for comparison
