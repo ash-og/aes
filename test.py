3d6a920483e2470a3445bcdc0afac1992bc92994
ash-og <aisling.young4@gmail.com>
2024-03-29 15:01:27 +0000

    initial test code

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 import unittest
 import ctypes
+from aes import sub_bytes as python_sub_bytes
 
 rijndael = ctypes.CDLL('./rijndael.so')
 
@@ -9,7 +10,11 @@ block = ctypes.create_string_buffer(buffer)
 
 rijndael.sub_bytes(block)
 
-result = ctypes.string_at(
-    rijndael.aes_encrypt_block(plaintext, key),
-    16
+result_c = ctypes.string_at(
+    rijndael.aes_encrypt_block(plaintext, key), 16
 )
+
+result_p = python_sub_bytes(block)
+
+print("c results: ", result_c)
+print("python results: ", result_p)
